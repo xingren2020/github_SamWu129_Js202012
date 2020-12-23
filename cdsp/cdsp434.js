adaf88f5265b5892fe5f633a6e9ccd21ca9ecf14
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-23 11:28:23 +0800

    Update cdsp434.js

--- cdsp/cdsp434.js
+++ cdsp/cdsp434.js
@@ -88,7 +88,7 @@ function AutoRead() {
     return new Promise((resolve, reject) => {
        let myrequest = {
             url: articleurl,
-            headers: articlehd,
+            headers: {"Accept": "*/*","Accept-Encoding": "gzip, deflate","Accept-Language": "zh-Hans-CN;q=1","As-Version": "v1","Content-Length": "256","Device-Code": "DB39CBD2-B832-4284-888A-FC71496B0855","Dtu": "appStore","Host": "api-ddvideo.1sapp.com","Lat": "0.000000","Lon": "0.000000","Mobile-Brand": "iPhone","Mobile-Model": "iPhone 5s","Network": "WIFI","Oaid": "78BE3580-7104-440C-BCB2-8AD09A3A9227","Os": "iOS","Os-Version": "10.3.3","Source": "appStore","TK": "ACLbOcvSuDJChIiK_HFJawhVxoUjtIKjf-ZkZHNw","Token": "ce6dgbCzRUBhUra8B69QHOJecQMkePXsbpA7idijFSACag4nX11daQioEXFxUtnu2PAoQzET-UHW988O3H9cPvtWc-2K79bml2IEblY7bT7FP6riDAIR2XoPqI7mvH5ACKRhhvj-Ci9dGwognXxnXJAEgesUX-A","Tuid": "2znL0rgyQoSIivxxSWsIVQ","User-Agent": "cai dan shi pin/1211 (iPhone; iOS 10.3.3; Scale/2.00)","Version": "1211","Version-Name": "",},
             body: JSON.stringify(articlebd)
         };
   console.log(`URL:`+articleurl)
