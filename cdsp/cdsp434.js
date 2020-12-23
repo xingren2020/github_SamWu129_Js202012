a9b029e19003cde72681b1034aafaec020d4f529
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-23 21:13:38 +0800

    Update cdsp434.js

--- cdsp/cdsp434.js
+++ cdsp/cdsp434.js
@@ -112,7 +112,7 @@ function AutoRead() {
            //if (readres.code == '0') {
             //console.log(`\n本次阅读获得${readres.data.reward_value}个金币，请等待3s后执行下一个账号阅读\n`);
          //  await $.wait(3000);
-            }
+          //  }
           }
            catch(error) {   
                //let readres = JSON.parse(data);
