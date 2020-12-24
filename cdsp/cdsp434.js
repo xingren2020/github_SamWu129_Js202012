04231dd26c4fd80a879fe66f09da13a149b4358a
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-24 20:38:16 +0800

    Update cdsp434.js

--- cdsp/cdsp434.js
+++ cdsp/cdsp434.js
@@ -117,7 +117,7 @@ function AutoRead() {
               //"Host": "api-ddvideo.1sapp.com",
               "TK": articleTK,//"ACLbOcvSuDJChIiK_HFJawhVxoUjtIKjf-ZkZHNw",
               "Token": articleToken,//"2a79A1FjK4LLsNRrLBn0ilLELUADWupTgwDfP0tczxEstMYStbkAyL59ePMGhUbXOfgRYVoX0NdFuJD3HTHoqZF--3HUc-3kIfJqX3UJzDfPA_3IDqbdEOIEX1HsaPhknakWHTpUSLJDk6hD-sgd2ar8eoep-ak",
-              "User-Agent": "cai dan shi pin/1211 (iPhone; iOS 10.3.3; Scale/2.00)"
+              //"User-Agent": "cai dan shi pin/1211 (iPhone; iOS 10.3.3; Scale/2.00)"
             },
             body: articlebd
         };
@@ -135,7 +135,7 @@ function AutoRead() {
    // $.done();*/
           try{
            let readres = JSON.parse(data);
-            console.log(readres)
+           // console.log(readres)
            if (readres.code == '0') {
             console.log(`\n本次阅读获得${readres.data.reward_value}个金币，请等待1s后执行下一个账号阅读\n`);
             await $.wait(1000);
