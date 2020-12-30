4345703cab8976c7365c5dfc81a2120fb48c1570
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-30 21:38:26 +0800

    Update youth_read_qf_1.js

--- youth/youth_read_qf_1.js
+++ youth/youth_read_qf_1.js
@@ -28,6 +28,8 @@ let ReadArr = [], YouthBody = "",readscore = 0;
           ReadArr.push(YouthBody[item])
         }
     })
+
+      console.log(`============ 共${ReadArr.length}个账号  =============\n`)
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  !(async () => {
