510864e738f851c7f752a04129cbd37623024b84
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-31 21:03:21 +0800

    Update youth_read_rl.js

--- youth/youth_read_rl.js
+++ youth/youth_read_rl.js
@@ -5,7 +5,7 @@
 
 //let s = 30000 //等待延迟30s
 const $ = new Env("【youth_read_rl】")
-//const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 let ReadArr = [], YouthBody = "",readscore = 0;
   if (process.env.YOUTH_READ && process.env.YOUTH_READ.indexOf('&') > -1) {
   YouthBody = process.env.YOUTH_READ.split('&');
