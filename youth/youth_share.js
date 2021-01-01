a00e34df64b9a364949358f7ad1f76014344634a
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-01 20:50:17 +0800

    Update youth_share.js

--- youth/youth_share.js
+++ youth/youth_share.js
@@ -50,7 +50,7 @@ if (process.env.YOUTH_HD_SHARE && process.env.YOUTH_HD_SHARE.indexOf('\n') > -1)
 function share() {
     time = new Date().getTime();
     str = randomWord(false,32);
-    articleurl = `https://script.baertt.com/count2/callback?si=${str}&_=${time}&jsonpcallback=jsonp4`；
+    articleurl = "https://script.baertt.com/count2/callback?si="+str+"&_="+time+"&jsonpcallback=jsonp4";
     return new Promise((resolve, reject) => {
        let myrequest = {
             url: articleurl,
