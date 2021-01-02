8648e7299587615e4733be895e7bd7e3a761084f
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-03 07:10:33 +0800

    Update txnews.js

--- txnews/txnews.js
+++ txnews/txnews.js
@@ -106,7 +106,7 @@ if (isGetCookie) {
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}=============\n`)
      }
-  for (let j = 0; j < 5; j++) {     
+  for (let j = 0; j < 6; j++) {     
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookieVal = cookiesArr[i];
