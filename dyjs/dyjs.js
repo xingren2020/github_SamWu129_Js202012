e3f62343acfcf7cc9c1f5f6f8b902129b2013e5e
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-06 09:45:16 +0800

    Update dyjs.js

--- dyjs/dyjs.js
+++ dyjs/dyjs.js
@@ -139,13 +139,13 @@ if (process.env.DYJS_S_HD && process.env.DYJS_S_HD.indexOf('\n') > -1) {
       account = Account[i];
      console.log(`【开启任务】开始执行账号${account}的任务`);
      detail = ``;
-   for (let j = 0; j < BDArr.length; j++) {
+   for (let j = 0; j < 3; j++) {
       AppBD = BDArr[j];
     if(j==0 && $.time('HH')==21)  await sign();
     else if(j==1)  for (let k = 0; k < 3; k++) {
     await video();
      }
-    else if(j==3)  await info();
+    else if(j==2)  await info();
      }
      }
      console.log(`⏱⏱⏱执行下一个账号任务⏱⏱⏱`);
