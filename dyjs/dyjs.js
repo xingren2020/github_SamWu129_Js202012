f8973229e3a0a3762771c5ee231996156fe7d0a1
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-06 09:50:21 +0800

    Update dyjs.js

--- dyjs/dyjs.js
+++ dyjs/dyjs.js
@@ -156,7 +156,7 @@ if (process.env.DYJS_S_HD && process.env.DYJS_S_HD.indexOf('\n') > -1) {
      console.log(`⏱⏱⏱执行下一轮⏱⏱⏱`);
     // await $.wait(120000);    
    }  
-     if($.time('HH')==23 || 11 {
+     if($.time('HH')==23 || 11) {
     await notify.sendNotify($.name+'|'+account, detail)
      }
      console.log(`🎉🎉🎉运行结束🎉🎉🎉`)
