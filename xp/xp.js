e4d65c66009cbe81a89d6680793c552b57948d34
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-13 13:17:22 +0800

    Update xp.js

--- xp/xp.js
+++ xp/xp.js
@@ -83,7 +83,7 @@ let detail = ``;
     }
      await $.wait(30000);
  }
- else if (h==1 && $.time('HH')==3||h==1 && $.time('HH')==13)  for (let i = 0; i < HDArr.length; i++) {
+ else if (h==1 && $.time('HH')==8||h== && $.time('HH')==9)  for (let i = 0; i < HDArr.length; i++) {
     if (HDArr[i]) {
       articleHD = HDArr[i];
       account = Account[i];
@@ -93,7 +93,7 @@ let detail = ``;
      }
      await $.wait(1000);
     }
- else if (h==2 && $.time('HH')==10||h==2 && $.time('HH')==11) for (let i = 0; i < HDArr.length; i++) {
+ else if (h==2 && $.time('HH')==8||h==2 && $.time('HH')==9) for (let i = 0; i < HDArr.length; i++) {
     if (HDArr[i]) {
       articleHD = HDArr[i];
       account = Account[i];
