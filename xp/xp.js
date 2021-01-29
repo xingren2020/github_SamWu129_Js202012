32bb0e8b83e1371d5e40b4c6ed9d9649fd9b93e4
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-01-29 21:03:24 +0800

    Update xp.js

--- xp/xp.js
+++ xp/xp.js
@@ -127,7 +127,7 @@ if (!videoheaderArr[0]) {
       //await profit()
       //await balance()
       //await status()
-      //await getliveid()
+      await getliveid()
       //await control()
       //await withdraw()
       await watch_livevideo()
@@ -313,7 +313,7 @@ function getliveid() {
 return new Promise((resolve, reject) => {
   let getliveidurl ={
     url: `https://veishop.iboxpay.com/nf_gateway/nf_content_service/live/ignore_tk/v1/query_living_list_id.json`,
-    headers: JSON.parse(headers)
+    headers: JSON.parse(videoheader)
 }
    $.get(getliveidurl,(error, response, data) =>{
      const result = JSON.parse(data)
@@ -329,8 +329,8 @@ return new Promise((resolve, reject) => {
 //livevideo
 function watch_livevideo() {
 //let liveids = liveid.replace(/\d{3}$/,Math.round((Math.random() > 0.1 ? Math.random() : (Math.random()+0.1)) *1000));
-    let str = randomWord(false,15);
-    let liveids = '1355'+str;
+    //let str = randomWord(false,15);
+    //let liveids = '1355'+str;
 $.log('livesid:'+liveids)
 return new Promise((resolve, reject) => {
   let watch_livevideourl ={
